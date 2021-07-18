f23974ee3a6cc7e7c2cbb4318109291e2d3fc91d
lth75 <lth75@qq.com>
2021-07-18 13:20:39 +0800

    fix id offset, pass 1st sample

--- P1081.cpp
+++ P1081.cpp
@@ -72,7 +72,7 @@ void preProcess()
 	{
 		idx_arr[cities[i].id] = i;
 	}
-	cities[n].h = (long long) 10e10;
+	cities[n].h = (long long) 1e11;
 	cities[n].next = n;
 	cities[n].prev = n;
 	cities[0].next = 1;
@@ -84,7 +84,7 @@ void preProcess()
 		cities[i].prev = i - 1;
 		cities[i].next = i + 1;
 	}
-	printCities();
+	// printCities();
 	// build a and b
 	for (int i = 0; i < n; i++)
 	{
@@ -160,9 +160,35 @@ void printArrF(int k)
 	}
 }
 
+void printFax(int k)
+{
+	for (int level = 0; level < k; level++)
+	{
+		printf("fa level %d:", level);
+		for (int i = 0; i <= n; i++)
+		{
+			printf("(%d,%lld),", cities[i].id, fax[level][i]);
+		}
+		printf("\n");
+	}
+}
+
+void printFbx(int k)
+{
+	for (int level = 0; level < k; level++)
+	{
+		printf("fb level %d:", level);
+		for (int i = 0; i <= n; i++)
+		{
+			printf("(%d,%lld),", cities[i].id, fbx[level][i]);
+		}
+		printf("\n");
+	}
+}
+
 void build_2tree()
 {
-	printAnBn();
+	// printAnBn();
 	for (int i = 0; i <= n; i++)
 	{
 		f[0][i] = an[i];
@@ -179,24 +205,35 @@ void build_2tree()
 			f[level][i] = f[level - 1][f[level - 1][i]];
 			fax[level][i] = fax[level - 1][i] + fax[level - 1][f[level - 1][i]];
 			fbx[level][i] = fbx[level - 1][i] + fbx[level - 1][f[level - 1][i]];
+			if (fax[level][i] > 1e10)
+				fax[level][i] = 1e10;
+			if (fbx[level][i] > 1e10)
+				fbx[level][i] = 1e10;
 		}
 	}
-	printArrF(3);
+	//printArrF(5);
+	//printFax(5);
+	//printFbx(5);
 }
 
-void getab(long long &a, long long &b, long long x, int s)
+void getab(long long &a, long long &b, long long x, int sc)
 {
 	a = 0;
 	b = 0;
-	for (int i = 17; i >= 0; i--)
+	// s = idx_arr[s];
+	// printf("sc:%d,id:%d\n", sc, cities[sc].id);
+	for (int i = 16; i >= 0; i--)
 	{
-		if (a + b + fax[i][s] + fbx[i][s] < x)
+		if ( (a + b + fax[i][sc] + fbx[i][sc] <= x) && (f[i][sc] < n) )
 		{
-			a += fax[i][s];
-			b += fbx[i][s];
-			s = f[i][s];
+			a += fax[i][sc];
+			b += fbx[i][sc];
+			//printf("level:%d,a:%lld,b:%lld,ins:%d,outs:%d\n", i,
+			//       a, b, cities[sc].id, cities[f[i][sc]].id);
+			sc = f[i][sc];
 		}
 	}
+	// printf("x:%lld, s:")
 }
 
 int main()
@@ -213,11 +250,16 @@ int main()
 		long long a, b;
 		int sx = i;
 		getab(a, b, x0, sx);
-		if (b == 0)
-			ratio = 1e15;
+		//ratio = (b < 1LL) ? 1e14 : (1.0 * a ) / b;
+		if (b < 1LL)
+		{
+			ratio = 1e14;
+			//printf("%lld<1,ratio=%le\n", b, ratio);
+		}
 		else
 		{
-			ratio = 1.0 * a / b;
+			ratio = (1.0 * a ) / b;
+			//printf("%lld,ratio=%le\n", b, ratio);
 		}
 		if (min_ratio >= ratio)
 		{
@@ -225,11 +267,12 @@ int main()
 			min_i = i;
 		}
 	}
-	printf("%d\n", cities[min_i].id);
+	printf("%d\n", cities[min_i].id + 1);
 	for (int i = 0; i < m; i++)
 	{
 		long long a, b;
-		getab(a, b, x[i], s[i]);
+		int idx = idx_arr[s[i] - 1];
+		getab(a, b, x[i], idx);
 		printf("%lld %lld\n", a, b);
 	}
 	return 0;
